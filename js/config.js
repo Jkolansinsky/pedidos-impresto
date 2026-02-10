b21b0862b6b1d1807116ea0537eee46a8d73e7e0
Jkolansinsky <intranet.impresto@gmail.com>
2026-02-09 18:14:08 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ============================================
 
 // URL de tu Google Apps Script (CAMBIAR POR LA TUYA)
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwcGg2PTnPuIezvKBYlDw4IZ3iIPocGcG11yEAaDnP4oQ2ykoAKtorMCefVxyIl1t3_MA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz7kuELh-tdObnHiZVA1Q0NG7GfI6eGetD5hfYXosIZ1RzHTU7kVQ5049BHW9EcZqTbkg/exec';
 
 // Variables globales de geolocalizaciÃ³n
 let userCurrentLocation = null;
@@ -256,6 +256,7 @@ window.addEventListener('beforeunload', function() {
         navigator.geolocation.clearWatch(geoWatchId);
     }
 });
+
 
 
 
